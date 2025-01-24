1912f2ca447b0903b8eee53b6cac90b19eb8776a
Your Name <youremail@example.com>
2025-01-24 23:54:10 +0530

    Filename Changed

--- Introduction_to_Pandas/15.py
+++ Introduction_to_Pandas/15.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
-def meltTable(report: pd.DataFrame) -> pd.DataFrame:
-    result = pd.melt(report, id_vars=["product"], var_name="quarter", value_name="sales")
-    return result
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals['weight'] > 100].sort_values('weight', ascending=False)[['name']]
