62505969e4e63a7ada02617a4b0d275ea96ceec7
Your Name <mohamed.irsath.mail@gmail.com>
2025-01-30 23:23:31 +0530

    solus

--- Programming_Skills/28.cpp
+++ Programming_Skills/28.cpp
@@ -1,10 +1,28 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int numb1 = Integer.parseInt(num1);
-        int numb2 = Integer.parseInt(num2);
+        if (num1.equals("0") || num2.equals("0")) return "0";
 
-        int sum = numb1*numb2;
-        String str = String.valueOf(sum);
-        return str;
+        int n = num1.length();
+        int m = num2.length();
+        int[] result = new int[n + m];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        
+        return sb.toString();
     }
 }
