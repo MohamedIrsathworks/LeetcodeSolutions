4dde9cdaa5f6349395b51b8a12b9d598092c93d0
Your Name <mohamed.irsath.mail@gmail.com>
2025-02-01 23:25:29 +0530

    Solus

--- Algorithms/Solution.java
+++ Algorithms/Solution.java
@@ -1,36 +1,17 @@
 public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+            
+            rev = rev * 10 + digit;
         }
-
-        int m = nums1.length, n = nums2.length;
-        int leftSize = (m + n + 1) / 2;
-        int low = 0, high = m;
-
-        while (low <= high) {
-            int partitionX = (low + high) / 2;
-            int partitionY = leftSize - partitionX;
-
-            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
-            int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
-
-            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
-            int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
-
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
-                } else {
-                    return Math.max(maxLeftX, maxLeftY);
-                }
-            } else if (maxLeftX > minRightY) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
-            }
-        }
-        
-        throw new IllegalArgumentException("Invalid input");
+        return rev;
     }
+}
+ {
+    
 }
